2d43a4ce72ccf638c9e7c02251c19d95de08261b
Bwiree <solomonbwire@gmail.com>
2018-08-30 13:58:38 +0300

    modify users,questions,answers,views,dbase and .gitignore files

---
+++ my_api/auth/views.py
@@ -0,0 +1,116 @@
+from flask import Blueprint, request, Response, jsonify, make_response
+from flask_restful import Resource,Api
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
+from my_api.models.dbase import DatabaseConnection
+import json
+
+auth_blueprint = Blueprint('auth', __name__, url_prefix='/api/v1')
+api = Api(auth_blueprint)
+
+database_connection = DatabaseConnection()
+
+
+class QuestionsBox(Resource):
+    def get(self):
+        question_dict = database_connection.get_all_questions()
+        if question_dict:
+            return make_response(jsonify(question_dict), 200)
+        return Response(json.dumps(['No questions to display!']), status=404, mimetype='application/json')
+
+    def post(self):
+            data = request.get_json()
+            author = data['author']
+            body = data['body']
+            if isinstance(body, str) and not body.isspace() and len(author) > 0:
+                database_connection.create_question(body,author)
+                return Response(json.dumps(['Question added successfully']), status=201, mimetype='application/json')
+            return Response(json.dumps(['All fields should not be empty']), status=404, mimetype='application/json')
+
+
+api.add_resource(QuestionsBox, '/questions')
+
+
+class RegisterAPI(Resource):
+    def post(self):
+        if not request.is_json:
+            return jsonify({
+                'message': 'json missing'
+            }),400
+        email = request.args.get('email', None)
+        username = request.args
+        password = request.args.get('password', None)
+        confirm_password = request.args.get('confirm_password', None)
+        if not email:
+            return jsonify({
+                'message': 'email address missing!'
+            }), 400
+        elif not username:
+            return jsonify({
+                'message': 'user name required'
+            }),400
+        elif not password:
+            return jsonify({
+                'message': 'please provide your password!'
+            }), 400
+        elif not confirm_password:
+            return jsonify({
+                'message': 'please confirm your password!'
+            }), 400
+        if len(username) > 1:
+            if confirm_password == password:
+                database_connection.register(email,username,password)
+                return {'message': 'User registered successfully'}, 201
+
+
+api.add_resource(RegisterAPI, '/auth/register')
+
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        user, password_hash = database_connection.get_user(username)
+        if user and database_connection.confirm_password_hash(password, password_hash):
+            print('The user is confirmed')
+            access_token = create_access_token(identity=username)
+            return {'message': 'Token created',
+                    'access_token': access_token}, 201
+        return {'message': 'Invalid username or password'}, 400
+
+
+api.add_resource(Login, '/auth/Login')
+
+
+
+
+class Question(Resource):
+    def get(self, questionId):
+        question_dict = database_connection.get_specific_question(questionId)
+        if question_dict:
+           return make_response(jsonify(question_dict), 200)
+
+        return Response(json.dumps(['Question does not exist']), status=404, mimetype='application/json')
+
+    def delete(self, questionId):
+        database_connection.delete_questions(questionId)
+        return {'message': 'Question {} deleted'.format(questionId)}, 204
+
+
+api.add_resource(Question, '/questions/<int:questionId>')
+
+
+class AddAnswer(Resource):
+    def post(self, questionId):
+        data = request.get_json()
+        body = data['body']
+        author = data['author']
+        questionId = data['questionId']
+        if isinstance(body, str) and not body.isspace() and len(body) > 0:
+            database_connection.answer_question(body,author,questionId), 201
+            return Response(json.dumps(['Answer added successfully']), status=201, mimetype='application/json')
+        return Response(json.dumps(['All fields must not be empty']), status=404, mimetype='application/json')
+
+
+api.add_resource(AddAnswer, '/questions/<int:questionId>/answers')
+
--- my_api/models/dbase.py
+++ my_api/models/dbase.py
@@ -1,48 +1,177 @@
 import psycopg2
+from pprint import pprint
+from passlib.hash import pbkdf2_sha256 as sha256
+from my_api.models.questions import Question
+from my_api.models.users import User
+from my_api.models.answers import Answer
 
 
-class DatabaseConnection:
+class DatabaseConnection(object):
     def __init__(self):
         try:
             self.connection = psycopg2.connect(
-                "dbname='stackoverflow' user='postgres' host='localhost' password='boy' port='5432'")
+                "dbname='stackoverflow' "
+                "user='postgres' "
+                "host='localhost'"
+                " password='boy' "
+                "port='5432'"
+            )
             self.connection.autocommit = True
             self.cursor = self.connection.cursor()
+            self.queries = []
 
-        except:
-            print("Could not connect to server")
+        except (Exception, psycopg2.DatabaseError) as e:
+            pprint(e, "Could not connect to server")
 
     def create_users_table(self):
-        create_table_command = """CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,  
-        email VARCHAR(100) NOT NULL, 
-        username VARCHAR(30),
-        password VARCHAR(100) NOT NULL
-        )"""
-        self.cursor.execute(create_table_command)
+        try:
+            create_table_command = """CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,  
+            email VARCHAR(100) NOT NULL, 
+            username VARCHAR(30),
+            password VARCHAR(100) NOT NULL
+            )"""
+            self.cursor.execute(create_table_command)
 
-    def insert_record(self, tablename, ):
-        record = ("Juma", "saffgdd@gmail.com", "jsohjhhrn")
-        insert_command = "INSERT INTO users(email, username, password) VALUES('" + record[0] + "','" + record[1] + "','" + record[2] + "');"
-        print(insert_command)
-        self.cursor.execute(insert_command)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
     def create_questions_table(self):
-        create_table_command = """CREATE TABLE IF NOT EXISTS questions(
-        questionId SERIAL PRIMARY KEY, 
-        question VARCHAR(150), 
-        author VARCHAR(50), 
-        create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );"""
-        self.cursor.execute(create_table_command)
+        try:
+            create_table_command = """CREATE TABLE IF NOT EXISTS questions(
+            questionId SERIAL PRIMARY KEY, 
+            userId int,
+            body VARCHAR(500), 
+            author VARCHAR(50), 
+            create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
+            );"""
+            self.cursor.execute(create_table_command)
+
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
     def create_answers_table(self):
-        create_table_command = """CREATE TABLE IF NOT EXISTS answers(
-        questionId serial PRIMARY KEY, 
-        author VARCHAR(50), 
-        answer VARCHAR(150) 
-        );"""
-        self.cursor.execute(create_table_command)
+        try:
+            create_table_command = """CREATE TABLE IF NOT EXISTS answers(
+            answerId serial PRIMARY KEY,
+            questionId INT,
+            body VARCHAR(150),
+            author VARCHAR(50),
+            accept_status boolean DEFAULT FALSE,
+            published_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );"""
+            self.cursor.execute(create_table_command)
+
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def drop_table(self, *table_names):
+        for table_name in table_names:
+            drop_table = "DROP TABLE IF EXISTS {} CASCADE".format(table_name)
+            pprint("Tables dropped")
+            self.cursor.execute(drop_table)
+
+    def register(self,email, username, password):
+        password = self.hash_password(password)
+        user = User(email, username, password)
+        insert_command = "INSERT INTO users(email, username, password) VALUES('{}' ,'{}','{}')".format(user.email, user.username, user.password)
+        pprint(insert_command)
+        self.cursor.execute(insert_command)
+
+    def get_user(self, username):
+        try:
+
+            sql_command = "SELECT *FROM users WHERE username = '{}'".format(username)
+            self.cursor.execute(sql_command)
+            user = self.cursor.fetchone()
+            email = user[1]
+            username = user[2]
+            password = user[3]
+            return email, username, password
+        except Exception as e:
+            # print({'error': 'User not found {}'.format(e)})
+            return {'error': 'User not found {}'.format(e)}
+
+    def create_question(self, body, author):
+        user_sql = "SELECT id FROM users WHERE username = '{}'".format('Bwire')
+        self.cursor.execute(user_sql)
+        user = self.cursor.fetchone()
+
+        questions = Question(body,author)
+        sql_command = "INSERT INTO questions(userId, body,author) VALUES ('{}','{}','{}')".format(user[0], questions.body, questions.author)
+        pprint(sql_command)
+        self.cursor.execute(sql_command)
+
+    def query_all_questions(self):
+        queries = []
+        try:
+            sql_command = "SELECT *FROM questions"
+            self.cursor.excute(sql_command)
+            items = self.cursor.fetchall()
+            if items:
+                for item in items:
+                    queries.append(item)
+                return queries
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        else:
+            return queries
+
+    def get_all_questions(self):
+        sql = "SELECT *FROM questions"
+        self.cursor.execute(sql)
+        rows = self.cursor.fetchall()
+        questions = [questions for questions in rows]
+        return {'all_questions': questions}
+
+    def get_all_answers(self):
+        self.cursor.execute("SELECT * FROM answers")
+        answers = self.cursor.fetchall()
+        return answers
+
+    def get_specific_question(self, questionId):
+        try:
+            question_sql = "SELECT *FROM questions WHERE questionId = %s "
+            self.cursor.execute(question_sql, (questionId,))
+            question = self.cursor.fetchone()
+            print(question)
+            answers_sql = "SELECT * FROM answers WHERE questionId = %s "
+            self.cursor.execute(answers_sql, (questionId,))
+            answers = self.cursor.fetchall()
+            answersList = [row for row in answers]
+
+            return {'question': {'author': question[3],
+                                 'body': question[2],
+                                 'answers': answersList
+                                 }}
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def answer_question(self, questionId, body, author):
+        answers = Answer(questionId, body, author)
+        sql = "INSERT INTO answers(questionId, body, author) VALUES (%s, %s, %s);"
+        print(sql)
+        self.cursor.execute(sql, (answers.questionId, str(answers.body), answers.author))
+
+    def hash_password(self, password):
+        return sha256.hash(password)
+
+    def confirm_password_hash(self, password, password_hash):
+        return sha256.verify(password, password_hash)
+
+    def delete_questions(self, questionId):
+        sql = "DELETE FROM questions WHERE  questionId = {} ".format(
+            questionId)
+        rows_deleted = self.cursor.rowcount
+        print(rows_deleted)
+        self.cursor.execute(sql)
+        return {"message": "Question {} deleted".format(questionId)}
+
+    def update(self, accept_status, answerId):
+        sql = 'UPDATE answers SET accept_status = {} WHERE answerId = {}'.format(accept_status, answerId)
+        self.cursor.execute(sql)
+        return {'message': 'Answer status updated'}
 
 
 if __name__ == '__main__':
@@ -50,4 +179,7 @@ if __name__ == '__main__':
     connect.create_users_table()
     connect.create_questions_table()
     connect.create_answers_table()
-    connect.insert_record()
+    connect.register(email="solomonbwire@gmail.com",username="Bwire",password="nodsbsbn" )
+    connect.create_question(body="What is programming",author="Jackie")
+    connect.get_all_questions()
+    # connect.delete_questions(questionId=1)
